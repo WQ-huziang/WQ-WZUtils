6883e6b23abf9b191b62938163522ca7fa4bf27b
luoqingming <390571270@qq.com>
2018-03-19 19:59:20 +0800

    merged master

--- WZPiper/inc/WZPiper.h
+++ WZPiper/inc/WZPiper.h
@@ -7,15 +7,6 @@
 class WZPiper {
 
  public:
-<<<<<<< HEAD
-   virtual void init_as_server() {}
-   virtual void set_config_info(char file_path[256]) {}
-   virtual void init_as_client() {}
-   virtual Frame do_read() {}
-   virtual void do_write(Frame mail) {}
-   virtual void handle_event() {}
-   virtual void handle_accept() {}
-=======
 
    virtual void init_as_server() {}
    virtual void set_config_info(char file_path[256]) {}
@@ -32,9 +23,7 @@ class WZPiper {
    virtual void set_event_fd(int sockfd) {} 
    virtual int get_event_fd(int pos) {}
    virtual int get_listen_fd() {}
-   //...
 
->>>>>>> 54eb8959bc86f2fb54e47fb9df50b6d9db56c7bd
  private:
    
 };
