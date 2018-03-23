c77e1c0cf60b0d4efd209e939b97781c65b76349
luoqingming <390571270@qq.com>
2018-03-23 11:30:28 +0800

    add CTparser.h

---
+++ WZConfigReader/CTparser.h
@@ -0,0 +1,105 @@
+#ifndef CTPARSER_H_
+#define CTPARSER_H_
+
+#include <string>
+#include <cstring>
+#include "frame.h"
+#include "wzadapter/wzdatastruct.h"
+#include "wzadapter/transportstruct.h"
+#include "CTP/ThostFtdcUserApiStruct.h"
+
+inline struct CThostFtdcInputOrderField parserFrom (const TSInputOrderField req){
+	CThostFtdcInputOrderField rtn;
+	switch (req.OrderType){
+		case WZ_ORDER_MARKET_ORDER:
+			rtn.OrderPriceType = THOST_FTDC_OPT_AnyPrice;
+			rtn.TimeCondition = THOST_FTDC_TC_IOC;
+			rtn.VolumeCondition = THOST_FTDC_VC_AV;
+			rtn.ContingentCondition = THOST_FTDC_CC_Immediately;
+
+		case WZ_ORDER_LIMIT_ORDER:
+			rtn.OrderPriceType = THOST_FTDC_OPT_LimitPrice;
+			rtn.TimeCondition = THOST_FTDC_TC_GFD;
+			rtn.VolumeCondition = THOST_FTDC_VC_AV;
+			rtn.ContingentCondition = THOST_FTDC_CC_Immediately;
+
+		case WZ_ORDER_FOK_ORDER:
+			rtn.OrderPriceType = THOST_FTDC_OPT_LimitPrice;
+			rtn.TimeCondition = THOST_FTDC_TC_IOC;
+			rtn.VolumeCondition = THOST_FTDC_VC_CV;
+			rtn.ContingentCondition = THOST_FTDC_CC_Immediately;
+
+		case WZ_ORDER_FAK_ORDER:
+			rtn.OrderPriceType = THOST_FTDC_OPT_LimitPrice;
+			rtn.TimeCondition = THOST_FTDC_TC_IOC;
+			rtn.VolumeCondition = THOST_FTDC_VC_MV;
+			rtn.ContingentCondition = THOST_FTDC_CC_Immediately;
+
+		default :
+			rtn.OrderPriceType = THOST_FTDC_OPT_LimitPrice;
+			rtn.TimeCondition = THOST_FTDC_TC_GFD;
+			rtn.VolumeCondition = THOST_FTDC_VC_AV;
+			rtn.ContingentCondition = THOST_FTDC_CC_Immediately;
+
+	}
+	strcpy(rtn.InstrumentID, req.InstrumentID);
+	rtn.LimitPrice = req.LimitPrice;
+	rtn.VolumeTotalOriginal = req.Volume;
+	rtn.Direction = req.Direction;
+	sprintf(rtn.CombOffsetFlag, "%c", req.OffsetFlag);
+	return rtn;
+}
+
+inline struct CThostFtdcInputOrderActionField parserFrom (const TSOrderActionField req){
+	CThostFtdcInputOrderActionField rtn;
+	strcpy(rtn.InstrumentID, req.InstrumentID);
+	strcpy(rtn.OrderRef, req.OrderRef);
+	rtn.ActionFlag = req.ActionFlag;
+	rtn.LimitPrice = req.LimitPrice;
+	rtn.VolumeChange = req.VolumeChange;
+	return rtn;
+}
+
+inline struct CThostFtdcOrderField parserFrom(const TSRtnOrderField req){
+	CThostFtdcOrderField rtn;
+	strcpy(rtn.InstrumentID, req.InstrumentID);
+	strcpy(rtn.OrderRef, req.OrderRef);
+	rtn.LimitPrice = req.LimitPrice;
+	rtn.VolumeTraded = req.VolumeTraded;
+	rtn.VolumeTotal = req.VolumeTotal;
+	rtn.VolumeTotalOriginal = req.VolumeTotalOriginal;
+	rtn.Direction = req.Direction;
+	sprintf(rtn.CombOffsetFlag, "%c", req.OffsetFlag);
+	rtn.OrderStatus = req.OrderStatus;
+	return rtn;
+}
+
+
+inline struct TSMarketDataField parserFrom(const CThostFtdcDepthMarketDataField &req){
+	TSMarketDataField rtn;
+	strcpy(rtn.TradingDay, req.TradingDay);
+	strcpy(rtn.InstrumentID, req.InstrumentID);
+	rtn.LastPrice = req.LastPrice;
+	rtn.PreSettlementPrice = req.PreSettlementPrice;
+	rtn.PreClosePrice = req.PreClosePrice;
+	rtn.PreOpenInterest = req.PreOpenInterest;
+	rtn.OpenPrice = req.OpenPrice;
+	rtn.HighestPrice = req.HighestPrice;
+	rtn.LowestPrice = req.LowestPrice;
+	rtn.Volume = req.Volume;
+	rtn.Turnover = req.Turnover;
+	rtn.OpenInterest = req.OpenInterest;
+	rtn.ClosePrice = req.ClosePrice;
+	rtn.SettlementPrice = req.SettlementPrice;
+	rtn.UpperLimitPrice = req.UpperLimitPrice;
+	rtn.LowerLimitPrice = req.LowerLimitPrice;
+	strcpy(rtn.UpdateTime, req.UpdateTime);
+	rtn.UpdateMillisec = req.UpdateMillisec;
+	rtn.BidPrice1 = req.BidPrice1;
+	rtn.BidVolume1 = req.BidVolume1;
+	rtn.AskPrice1 = req.AskPrice1;
+	rtn.AskVolume1 = req.AskVolume1;
+	return rtn;
+}
+
+#endif
