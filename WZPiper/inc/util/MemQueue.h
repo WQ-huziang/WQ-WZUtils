d0c781582978a7a5f3805d58b3fb13aa1597431b
luoqingming <390571270@qq.com>
2018-03-30 21:44:19 +0800

    gtest for MemQueue passed

--- WZPiper/inc/util/MemQueue.h
+++ WZPiper/inc/util/MemQueue.h
@@ -4,86 +4,137 @@
 #include <atomic>
 #include <sched.h>
 #include <assert.h>
+#include <string.h>
+#include <iostream>
+
+// using std::cout;
+// using std::endl;
 using std::atomic_compare_exchange_weak;
 
 
 // #define MAX_READER_SIZE 20
+// #define MAX_QUEUE_SIZE 4
 
-template <typename ELEM_T, typename MAX_READER_SIZE>
+template <typename ELEM_T>
 class MemQueue{
 private:
+	// each reader's readIndex
+    // int m_readIndex_arr[MAX_READER_SIZE];
+    int *m_readIndex_arr;
 
-    int m_readIndex_arr[MAX_READER_SIZE];
-    int reader_num=-1;
-    volatile atomic<int> m_writeIndex;
-    volatile atomic<int> m_maxReadIndex;
-    volatile atomic<int> m_minReadIndex;
+    // the total number of reader start from 1
+    std::atomic<int> reader_num;
+
+    // the write index
+    std::atomic<int> m_write_index;
+
+    //the max can read index
+    std::atomic<int> m_max_read_index;
+
+    // the last waiting to read slot index
+    std::atomic<int> m_min_read_index;
+
+    // queue to contain the data
+    // ELEM_T m_theQueue[MAX_QUEUE_SIZE];
     ELEM_T *m_theQueue;
-    inline int countToIndex(unsigned int a_count);
+
+    // the slowest slot read time
+    std::atomic<int> slowest_read_time;
+
+    // transform the count number to real index of theQueue
+    int count_to_index(int a_count);
+
+    int MAX_READER_SIZE;
     int Q_SIZE;
     int count;
 
 public:
-    MemQueue(unsigned int size = 1024);
+    MemQueue(int queue_size = 1024, int reader_size = 2);
     virtual ~MemQueue();
 
     // add a reader to the queue
     int add_reader();
 
+    // write a data to the queue
     bool push(const ELEM_T &a_data);
-    bool pop(ELEM_T &a_data,read_num);
-    int size();
+
+    // read a data from the queue
+    bool pop(ELEM_T &a_data,int read_num);
+
+    int get_queue_size();
+
+    int get_reader_size();
+
+    int get_write_index();
+
+    int get_max_read_index();
+
+    int get_min_read_index();
+
+    int get_slowest_read_time();
+
 };
 
 
 // map the current a_count index to the real queue index
-template <typename ELEM_T, typename MAX_READER_SIZE>
-inline unsigned int MemQueue::countToIndex(unsigned int &a_count) {
+template <typename ELEM_T>
+int MemQueue<ELEM_T>::count_to_index(int a_count) {
     return (a_count%Q_SIZE);
 }
 
 // constructure
-template <typename ELEM_T, typename MAX_READER_SIZE>
-MemQueue::MemQueue(int &size) {
-    m_theQueue = new ELEM_T[size];
-    memset(m_theQueue, 0, size*sizeof(ELEM_T));
+template <typename ELEM_T>
+MemQueue <ELEM_T> ::MemQueue(int queue_size, int reader_size) {
+
+	m_readIndex_arr = new int[reader_size];
+	m_theQueue = new ELEM_T[queue_size];
+
+    memset(m_theQueue, 0, sizeof(m_theQueue));
     // set every readIndex as the max value
-    memset(m_readIndex_arr, -1, sizeof(MAX_READER_SIZE));
-    this->m_writeIndex = 0;
-    this->m_maxReadIndex = 0;
-    this->m_minReadIndex = 0;
-    this->Q_SIZE = size;
+    memset(m_readIndex_arr, -1, sizeof(m_readIndex_arr));
+
+    this->slowest_read_time = 0;
+    this->reader_num = 0;
+    this->m_write_index = 0;
+    this->m_max_read_index = 0;
+    this->m_min_read_index = 0;
+    this->MAX_READER_SIZE = reader_size;
+    this->Q_SIZE = queue_size;
     this->count = 0;
+
 }
 
 template <typename ELEM_T>
-MemQueue::~MemQueue() {
+MemQueue<ELEM_T>::~MemQueue() {
     delete[] m_theQueue;
 }
 
-template <typename ELEM_T, typename MAX_READER_SIZE>
-bool MemQueue::push(const ELEM_T &a_data) {
+template <typename ELEM_T>
+bool MemQueue<ELEM_T>::push(const ELEM_T &a_data) {
 
-    int currentWriteIndex;
+    int cur_writeIndex;
 
     do{
-        currentWriteIndex = m_writeIndex;
+        cur_writeIndex = m_write_index;
 
         // if the producer catch the slowest consumer
-        if  (countToIndex(currentWriteIndex+1) == countToIndex(m_minReadIndex)){
+        if  (count_to_index(cur_writeIndex) == count_to_index(m_min_read_index) && count!= 0){
             // the queue is full
             // the producer catch the consumer
             return false;
         }
+
     // add, get the right to write
-    }while(!atomic_compare_exchange_weak(&m_writeIndex, &currentWriteIndex, (currentWriteIndex+1)));
+    }while(!atomic_compare_exchange_weak(&m_write_index, &cur_writeIndex, (cur_writeIndex+1)));
 
     // write a_data to queue
-    memcpy(&m_theQueue[countToIndex(currentWriteIndex)], &a_data, sizeof(a_data));
+    memcpy(&m_theQueue[count_to_index(cur_writeIndex)], &a_data, sizeof(a_data));
+
+    // cout<<"m_theQueue= "<<m_theQueue[count_to_index(cur_writeIndex)] << "; count_to_index(cur_writeIndex) = " << count_to_index(cur_writeIndex) <<endl;
 
     // Consider that there is more than 1 producer thread
-    // commit, wait for the producer finish the push operation, reset the upper bound of the to currentWriteIndex + 1
-    while(!atomic_compare_exchange_weak(&m_maxReadIndex, &currentWriteIndex, (currentWriteIndex + 1))){
+    // commit, wait for the producer finish the push operation, reset the upper bound of the to cur_writeIndex + 1
+    while(!atomic_compare_exchange_weak(&m_max_read_index, &cur_writeIndex, (cur_writeIndex + 1))){
 	// sched_yield();
     // 死等
     }
@@ -91,32 +142,51 @@ bool MemQueue::push(const ELEM_T &a_data) {
     return true;
 }
 
-template <typename ELEM_T, typename MAX_READER_SIZE>
-bool MemQueue::pop(ELEM_T &a_data, int reader_num) {
-    int currentReadIndex;
+template <typename ELEM_T>
+bool MemQueue<ELEM_T>::pop(ELEM_T &a_data, int reader_id) {
+    int cur_readIndex, cur_max_readIndex, cur_read_time, nxt_read_time;
+
+    // the reader is not exist
+    if ( reader_id == -1 || reader_id >= reader_num) {
+    	return false;
+    }
+
     do{
-        currentReadIndex = m_readIndex_arr[reader_num];
-        currentMaxReadIndex = m_maxReadIndex;
+        cur_readIndex = m_readIndex_arr[reader_id];
+        cur_max_readIndex = m_max_read_index;
 
         // if the consumer catch the producer
-        if (countToIndex(currentReadIndex) == countToIndex(m_maxReadIndex)){
+        if (count_to_index(cur_readIndex) == count_to_index(cur_max_readIndex)){
             // the queue is empty
             // the consumer catch the producer
             return false;
         }
 
         // pop a data from queue 
-        memcpy(&a_data, &m_theQueue[countToIndex(currentReadIndex)], sizeof(a_data));
+        memcpy(&a_data, &m_theQueue[count_to_index(cur_readIndex)], sizeof(a_data));
+        // cout << "reader: " << reader_id << "pop: " << a_data << "from: " << cur_readIndex <<endl;
 
-        // have atomic problem, because it's possible many reader is the lowest one
-        while(!atomic_compare_exchange_weak(&m_minReadIndex, &currentReadIndex, (currentReadIndex + 1))){
-        }
+	    if(m_min_read_index == cur_readIndex) {
+	    	// cout<<"m_min_read_index == cur_readIndex = " << cur_readIndex << endl;
+	    	do{
+	    		// add, get the right to add read time
+		        cur_read_time = slowest_read_time;
+
+		    }while(!atomic_compare_exchange_weak(&slowest_read_time, &cur_read_time, (cur_read_time+1)));
+		    // if every reader reader the slot
+		    if(slowest_read_time % reader_num == 0 && slowest_read_time != 0) {
+		        // have atomic problem?
+		        m_min_read_index++;
+		        // pop decrease the count
+		        // have atomic problem?
+        		--count;
+		    }
+	    }
 
         // no atomic problem(every reader has a distinct reader number), asign it directly
-        m_readIndex_arr[reader_num] = currentReadIndex + 1;
+        m_readIndex_arr[reader_id] = cur_readIndex + 1;
 
-        // pop decrease the count
-        --count;
+        
 
 		return true;
 
@@ -127,19 +197,51 @@ bool MemQueue::pop(ELEM_T &a_data, int reader_num) {
     return false;
 }
 
-template <typename ELEM_T, typename MAX_READER_SIZE>
-int MemQueue::add_reader(){
 
-	// add reader, reader num
+// get a reader number when a reader add to read
+// return the reader number, or return -1 if reader is full
+template <typename ELEM_T>
+int MemQueue<ELEM_T>::add_reader(){
+
+	// add reader, reader num + 1
 	reader_num++;
-	m_readIndex_arr[reader_num] = m_minReadIndex;
-	return reader_num;
+	if(reader_num > MAX_READER_SIZE) {
+		return -1;
+	}
+	m_readIndex_arr[reader_num - 1] = m_min_read_index;
+	return reader_num-1;
 
 }
 
 template <typename ELEM_T>
-int MemQueue::size() {
+int MemQueue<ELEM_T>::get_queue_size() {
     return count;
 }
+
+template <typename ELEM_T>
+int MemQueue<ELEM_T>::get_reader_size() {
+    return reader_num;
+}
+
+template <typename ELEM_T>
+int MemQueue<ELEM_T>::get_write_index() {
+	return m_write_index;
+}
+
+template <typename ELEM_T>
+int MemQueue<ELEM_T>::get_max_read_index() {
+    return m_max_read_index;
+}
+
+template <typename ELEM_T>
+int MemQueue<ELEM_T>::get_min_read_index() {
+    return m_min_read_index;
+}
+
+template <typename ELEM_T>
+int MemQueue<ELEM_T>::get_slowest_read_time() {
+    return slowest_read_time;
+}
+
 
 #endif// MEMQUEUE_H_
---
+++ WZPiper/src/mem_src/test_memqueue.cpp
@@ -0,0 +1,60 @@
+#include "MemQueue.h"
+#include "gtest/gtest.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PRT(...) printf(__VA_ARGS__)
+
+
+TEST_F(QueueTest, IsEmptyInitially){
+	EXPECT_EQ(2,);
+	EXPECT_EQ(0,);
+}
+
+TEST_F(QueueTest, EnqueueWorks){
+	EXPECT_EQ(1,q_i.push(3));
+	EXPECT_EQ(0,q_i.push(4));
+}
+
+TEST_F(QueueTest, DequeueWorks){
+	EXPECT_EQ(1,q_i.pop(int_x,int_reader1));
+	EXPECT_EQ(2,int_x);
+	EXPECT_EQ(1,q_i.pop(int_x,int_reader2));
+	EXPECT_EQ(2,int_x);
+	EXPECT_EQ(1,q_i.pop(int_x,int_reader2));
+	EXPECT_EQ(1,int_x);
+	EXPECT_EQ(0,q_i.pop(int_x,int_reader2));
+
+	EXPECT_EQ(1,q_i.push(3));
+	EXPECT_EQ(1,q_i.push(4));
+	EXPECT_EQ(0,q_i.push(5));
+}
+
+TEST_F(QueueTest, EmptyqueuepopWorks){
+
+	EXPECT_EQ(0,q_d.pop(dou_x,dou_reader1));
+
+}
+
+int main(int argc, char* argv[]){
+	MemQueue<int> q_i;
+	MemQueue<double> q_d;
+	int int_reader1;
+	int int_reader2;
+	int dou_reader1;
+
+	int int_x;
+	double dou_x;
+
+	int_reader1 = q_i.add_reader();
+	int_reader2 = q_i.add_reader();
+
+	PRT("first push is:%d\n",q_i.push(1));
+	PRT("second push is:%d\n",q_i.push(2));
+
+
+	PRT("get_queue_size:%d\n",q_i.get_queue_size());
+	PRT("second push is:%d\n",q_d.get_queue_size());
+
+	return 0;
+}
