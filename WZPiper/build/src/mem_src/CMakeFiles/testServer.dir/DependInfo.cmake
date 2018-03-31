c99448cc17300ed241c37843d5b403b886d83c7d
luoqingming <390571270@qq.com>
2018-03-31 16:25:56 +0800

    .

---
+++ WZPiper/build/src/mem_src/CMakeFiles/testServer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luoqingming/coding/WZUtils/WZPiper/src/mem_src/testServer.cpp" "/home/luoqingming/coding/WZUtils/WZPiper/build/src/mem_src/CMakeFiles/testServer.dir/testServer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  "../inc/util"
+  "../inc/wzadapter"
+  "../inc/CTP"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/luoqingming/coding/WZUtils/WZPiper/build/src/mem_src/CMakeFiles/logger.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
