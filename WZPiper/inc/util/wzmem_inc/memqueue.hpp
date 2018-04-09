009192f6ddd0229bf90ed9c7b8da62c709799112
luoqingming <390571270@qq.com>
2018-04-09 20:53:59 +0800

    MemEngine as a class template, add readme for MemEngine and WZPiper

--- WZPiper/inc/util/wzmem_inc/memqueue.hpp
+++ WZPiper/inc/util/wzmem_inc/memqueue.hpp
@@ -28,7 +28,7 @@ using std::atomic_store;
 // here to define a QueueDataStruct?
 
 /***************************************************************************
-Description: single writer and multiple reader lockless queue with atomic,
+Description: single writer and multiple reader lockless queue template with atomic,
     typename ELEM_T:the type to put in the queue
     int queue_size: the maximum number of data can push, must be 2^n
     int reader_size:the maximum number of reader can add
@@ -58,10 +58,10 @@ public:
     Function: addReader
     Description: add a reader to the queue
     InputParameter: none
-    Return: if succeed return a available reader id, 
+    Return: if succeed return a available reader id(>=0), 
         else if the reader is full return -1
     *************************************************/ 
-    unsigned int addReader();
+    int addReader();
 
     /************************************************* 
     Function: push
@@ -139,7 +139,7 @@ private:
     unsigned int m_readIndex_arr[reader_size];
 
     // the total number of reader start from 1
-    volatile std::atomic<unsigned int> reader_num;
+    volatile std::atomic<int> reader_num;
 
     // the next index to write(next time right to this index)
     volatile std::atomic<unsigned int> m_write_index;
@@ -240,8 +240,6 @@ bool MemQueue<ELEM_T, queue_size, reader_size>::push(const ELEM_T &a_datum) {
     // write a_datum to queue
     memcpy(&m_data_queue[countToIndex(cur_writeIndex)], &a_datum, sizeof(a_datum));
 
-    // PRT("m_data_queue= %d; countToIndex(cur_writeIndex) = %d\n", m_data_queue[countToIndex(cur_writeIndex)], countToIndex(cur_writeIndex));
-
     // Consider that there is more than 1 producer thread
     // commit, wait for the producer finish the push operation, reset the upper bound of the to cur_writeIndex + 1
     while(!atomic_compare_exchange_weak(&m_max_read_index, &cur_writeIndex, (cur_writeIndex + 1))){
@@ -273,14 +271,12 @@ bool MemQueue<ELEM_T, queue_size, reader_size>::pop(ELEM_T &a_datum, int &reader
         // if (countToIndex(cur_readIndex) == countToIndex(cur_max_readIndex)){
         // cur_readIndex always slower than cur_max_readIndex, no need to use countToIndex
         if (cur_readIndex == cur_max_readIndex){
-            // the queue is empty
-            // the consumer catch the producer
+            // the queue is empty, the consumer catch the producer
             return false;
         }
 
         // pop a data from queue 
         memcpy(&a_datum, &m_data_queue[countToIndex(cur_readIndex)], sizeof(a_datum));
-        // PRT("reader: %d; pop: %d; from: %d\n", reader_id, a_datum, cur_readIndex);
 
         do{
             // add, get the right to add read time
@@ -289,25 +285,16 @@ bool MemQueue<ELEM_T, queue_size, reader_size>::pop(ELEM_T &a_datum, int &reader
 
         if(m_min_read_index == cur_readIndex) {
 
-            // PRT("m_min_read_index == cur_readIndex =  %d\n", cur_readIndex);
-            // PRT("read_time[%d] =  %d\n", countToIndex(cur_readIndex), read_time[countToIndex(cur_readIndex)]);
-
             // if every reader have read the slot
-            // when read_num is 2^n
-            // if(read_time[countToIndex(cur_readIndex)] & (reader_num - 1) == 0 && read_time[countToIndex(cur_readIndex)] != 0) {
-            // when read_num is not 2^n
-            // if(read_time[countToIndex(cur_readIndex)] % reader_num == 0 && read_time[countToIndex(cur_readIndex)] != 0) {
             if(cur_read_time + 1 == reader_num) {
 
                 // read_time[countToIndex(cur_readIndex)] = 0 has atomic problem?
                 atomic_store(&read_time[countToIndex(cur_readIndex)],(unsigned int) 0);
 
-                // pop decrease the count has atomic problem?
-                // --count;
+                // pop --count has atomic problem?
                 atomic_fetch_sub(&count, (unsigned int) 1);
 
                 // ++min_read_index has atomic problem?
-                // ++m_min_read_index;
                 atomic_fetch_add(&m_min_read_index, (unsigned int) 1);
             }
         }
@@ -318,7 +305,7 @@ bool MemQueue<ELEM_T, queue_size, reader_size>::pop(ELEM_T &a_datum, int &reader
         return true;
 
     }while(true);
-
+    
     // Something went wrong. it shouldn't be possible to reach here
     assert(0);
     return false;
@@ -328,11 +315,11 @@ bool MemQueue<ELEM_T, queue_size, reader_size>::pop(ELEM_T &a_datum, int &reader
 // get a reader number when a reader add to read
 // return the reader number, or return -1 if reader is full
 template <typename ELEM_T, int queue_size, int reader_size>
-unsigned int MemQueue<ELEM_T, queue_size, reader_size>::addReader(){
+int MemQueue<ELEM_T, queue_size, reader_size>::addReader(){
 
     // add reader, reader num + 1, 
     // has atomic problem?
-    unsigned int cur_reader_num;
+    int cur_reader_num;
     do{
         // add, get the right to add read time
         cur_reader_num = atomic_load(&reader_num);
--- WZPiper/src/mem_src/test_WR.cpp
+++ WZPiper/src/mem_src/test_WR.cpp
@@ -16,26 +16,39 @@ Date: 2018-04-2
 //#define PRT(...)
 #endif
 
+#ifndef DataType
+#define DataType Frame
+#endif
+
+#ifndef QueueSize
+#define QueueSize 2048
+#endif
+
+#ifndef MaxReaderSize
+#define MaxReaderSize 2
+#endif
+
 Logger * logger;
 
 class ServerClientTest : public ::testing::Test{
 protected:
-	WZPiper<MemEngine> * memServer;
-	WZPiper<MemEngine> * memClient1;
-	WZPiper<MemEngine> * memClient2;
+	
+	WZPiper<MemEngine<DataType, QueueSize, MaxReaderSize> >  * memServer;
+	WZPiper<MemEngine<DataType, QueueSize, MaxReaderSize> >  * memClient1;
+	WZPiper<MemEngine<DataType, QueueSize, MaxReaderSize> >  * memClient2;
 
 	virtual void SetUp(){
 		// init all
 		char file[] = "../doc/config.ini";
 		logger -> ParseConfigInfo(file);
 
-		memServer = new WZPiper<MemEngine>(WZ_PIPER_SERVER);
+		memServer = new WZPiper<MemEngine<DataType, QueueSize, MaxReaderSize> > (WZ_PIPER_SERVER);
 		memServer -> init(file);
 
-		memClient1 = new WZPiper<MemEngine>(WZ_PIPER_CLIENT);
+		memClient1 = new WZPiper<MemEngine<DataType, QueueSize, MaxReaderSize> > (WZ_PIPER_CLIENT);
 		memClient1 -> init(file);
 
-		memClient2 = new WZPiper<MemEngine>(WZ_PIPER_CLIENT);
+		memClient2 = new WZPiper<MemEngine<DataType, QueueSize, MaxReaderSize> > (WZ_PIPER_CLIENT);
 		memClient2 -> init(file);
 	}
 	virtual void TearDown(){
@@ -49,14 +62,14 @@ TEST_F(ServerClientTest, WriteAndReadWorks){
 
 	// test write
 	Frame sendFrame;
-	for (int i = 0; i<DataQueueSize ; i++ ) {
+	for (int i = 0; i<QueueSize ; i++ ) {
 		sendFrame.source = i;
 		sendFrame.msg_type = i;
 		sendFrame.error_id = WZ_ERROR_ID_SUCCESS;
 		sendFrame.rtn_type = i;
 		sendFrame.length = i;
 		EXPECT_EQ(0 ,memServer -> wzSend(sendFrame));
-		if(i<(DataQueueSize/2)) 	EXPECT_EQ(0,memClient1 -> wzSend(sendFrame));
+		if(i<(QueueSize/2)) 	EXPECT_EQ(0,memClient1 -> wzSend(sendFrame));
 		else 	EXPECT_EQ(0,memClient2 -> wzSend(sendFrame));
 	}
 
@@ -68,7 +81,7 @@ TEST_F(ServerClientTest, WriteAndReadWorks){
 	// test Reader1
 	Frame recvFrame;
 
-	for (int i = 0; i<DataQueueSize ; i++ ) {
+	for (int i = 0; i<QueueSize ; i++ ) {
 		EXPECT_EQ(0,memClient1 -> wzRecv(recvFrame));
 		EXPECT_EQ(i,recvFrame.source);
 		EXPECT_EQ(i,recvFrame.msg_type );
@@ -87,15 +100,15 @@ TEST_F(ServerClientTest, WriteAndReadWorks){
 	
 	// test catch Writer
 	EXPECT_EQ(-1,memClient1 -> wzRecv(recvFrame));
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.source);
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.msg_type );
+	EXPECT_EQ(QueueSize - 1,recvFrame.source);
+	EXPECT_EQ(QueueSize - 1,recvFrame.msg_type );
 	EXPECT_EQ(WZ_ERROR_ID_SUCCESS,recvFrame.error_id);
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.rtn_type);
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.length);
+	EXPECT_EQ(QueueSize - 1,recvFrame.rtn_type);
+	EXPECT_EQ(QueueSize - 1,recvFrame.length);
 	EXPECT_EQ(-1,memServer -> wzSend(sendFrame));
 
 	// test Reader2
-	for (int i = 0; i<DataQueueSize ; i++ ) {
+	for (int i = 0; i<QueueSize ; i++ ) {
 		EXPECT_EQ(0,memClient2 -> wzRecv(recvFrame));
 		EXPECT_EQ(i,recvFrame.source);
 		EXPECT_EQ(i,recvFrame.msg_type );
@@ -106,11 +119,11 @@ TEST_F(ServerClientTest, WriteAndReadWorks){
 
 	// test catch Writer
 	EXPECT_EQ(-1,memClient2 -> wzRecv(recvFrame));
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.source);
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.msg_type );
+	EXPECT_EQ(QueueSize - 1,recvFrame.source);
+	EXPECT_EQ(QueueSize - 1,recvFrame.msg_type );
 	EXPECT_EQ(WZ_ERROR_ID_SUCCESS,recvFrame.error_id);
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.rtn_type);
-	EXPECT_EQ(DataQueueSize - 1,recvFrame.length);
+	EXPECT_EQ(QueueSize - 1,recvFrame.rtn_type);
+	EXPECT_EQ(QueueSize - 1,recvFrame.length);
 
 	// test write
 	EXPECT_EQ(0,memServer -> wzSend(sendFrame));
