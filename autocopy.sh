eb213444bad2627e585e94fac241051061874017
Moustache <huzang@mail2.sysu.edu.cn>
2018-04-17 14:20:16 +0800

    add sh

--- autocopy.sh
+++ autocopy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pro_list = ()
+pro_list=()
 
 # read the project root path
 read -t 30 -p "Please input your project path (not inclue /) : " propath
@@ -19,29 +19,47 @@ function copyDir() {
   cd $propath/inc/WZUtils/$1
 
   # get lib and inc
-  if [ -d "inc" && -d "lib" ]
+  if [[ -d "inc" ]] && [[ -d "lib" ]]
   then
-    mv lib/* $propath/lib/
-    rm -rf !(inc)
+    mv lib/* ../../../lib
+    mv inc ../inc
+    rm -rf *
+    mv ../inc inc
     mv inc/* ./
     rm -rf inc
+  else
+    mv *.h ../
+    rm -rf *
+    mv ../*.h ./
   fi
 
   cd -
 }
 
 function getName() {
-  j = 0
+  j=0
   for file in `ls ./`
   do
     if [ -d ./$file ]
     then
-      pro_list[j] = $file
-      j = `expr $j + 1`
+      pro_list[j]=$file
+      j=`expr $j + 1`
       copyDir $file
     fi
   done   
 }
 
+function printMessage() {
+  echo ""
+  echo "Include dir:" 
+ 
+  for dir in ${pro_list[*]}
+  do
+    echo "INCLUDE_DIRECTORIES(inc/WZUtils/$dir)"
+  done
+  echo "Parse this test in your CMake!"
+}
+
 initPro
 getName
+printMessage
